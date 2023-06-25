aed6ee6a129f6915b3a83c4d39832fc0446f9566
kamarbaraka <kamar254baraka@gmail.com>
2023-06-25 20:54:39 +0300

    init
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
